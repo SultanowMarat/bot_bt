703b876b81ff505ddc9480606c9432856ec9e9b6
Sultanow Marat <sultanowmarat@gmail.com>
2024-01-15 16:37:22 +0500

    Базовая настройка бота была произведена

---
+++ answer.py
@@ -0,0 +1,13 @@
+main_answer = "Привет! Как мы можем помочь вам сегодня? /help\n"
+main_answer += "1. Я интересуюсь услугами по автоматизации бизнеса.\n"
+main_answer += "2. Хочу узнать о продуктах 1С для бизнеса.\n"
+main_answer += "3. Мне нужно обучение 1С.\n"
+main_answer += "4. Контакты\n"
+
+second = '<a href="/help">Нажми сюда</a>'
+
+# Мусор код
+# Включаем логирование, чтобы не пропустить важные сообщения
+# logging.basicConfig(level=logging.INFO,
+#                     format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+#                     filename=f'log_{datetime.date.today()}.txt')
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('hello world')
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import answer
+import conf
+
+bot = Bot(token=conf.API_TOKEN)
+dp = Dispatcher()
+
+
+async def on_startup():
+    await bot.send_message()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+
+    await message.answer(text=answer.main_answer)
+
+@dp.message(Command("help"))
+async def help(message: types.Message):
+
+    await message.answer(text=answer.main_answer)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
